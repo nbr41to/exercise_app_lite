1db7fb6568d3f1e8085be690b1b390976217c121
nbr41to <nbr.41to@gmail.com>
2020-08-28 20:21:32 +0900

    Updates

---
+++ precache-manifest.14cad2942f7aebf854af8efb4c4e0fce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8abc79906a8407290399aef769251ab",
+    "url": "/exercise_app_lite/index.html"
+  },
+  {
+    "revision": "7d05f55060cef5db17a0",
+    "url": "/exercise_app_lite/static/js/2.058fee59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exercise_app_lite/static/js/2.058fee59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339b98aa711efd6a328b",
+    "url": "/exercise_app_lite/static/js/main.944cb02b.chunk.js"
+  },
+  {
+    "revision": "929ec8b4efa7c7635050",
+    "url": "/exercise_app_lite/static/js/runtime-main.a47680b2.js"
+  },
+  {
+    "revision": "400032f49118176adc8f1c675129a3ac",
+    "url": "/exercise_app_lite/static/media/user1.400032f4.jpg"
+  },
+  {
+    "revision": "292728335dc11a83a915885a8140bafb",
+    "url": "/exercise_app_lite/static/media/user2.29272833.jpg"
+  },
+  {
+    "revision": "1d1b6a8c6ed80fe6dfd1043753fc747c",
+    "url": "/exercise_app_lite/static/media/user3.1d1b6a8c.jpg"
+  },
+  {
+    "revision": "b8b67c7777f68ff9179949d0ca6b0e95",
+    "url": "/exercise_app_lite/static/media/user4.b8b67c77.jpg"
+  }
+]);
